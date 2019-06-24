c04623305ea5bda7ea0c327e89352e10ffe8ad18
Dylan Davis <djdavis1420@gmail.com>
2019-06-24 10:43:58 -0500

    Implemented Custom Scopes for Portfolios

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id' => 'portfolios#show', as: 'portfolio_show'
+  get 'portfolios/angular' => 'portfolios#angular'
+  get 'portfolios/ruby-on-rails' => 'portfolios#ruby_on_rails'
 
   resources :blogs do
     member do
