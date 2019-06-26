3fcefc0a0096198d87c6c413c4f98af1c8b791d5
Dylan Davis <djdavis1420@gmail.com>
2019-06-25 19:09:40 -0500

    Extracted Session Source Tracker from Application Controller into Controller Concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
   include DeviseWhitelist
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:src] if params[:src]
-  end
+  include SetSource
 end
