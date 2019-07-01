faddf179fc9c63ea62b79b6304eeb91ce5455044
Dylan Davis <djdavis1420@gmail.com>
2019-07-01 11:19:51 -0500

    Temporary Refactor of Navigation Using Heredoc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,20 @@ module ApplicationHelper
   def copyright_generator
     DevcampTutorialViewTool::Renderer.copyright "Dylan Davis", "All Rights Reserved"
   end
+
+  def nav_helper(style, tag_type)
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style} #{active?(root_path)}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_path}" class="#{style} #{active?(about_path)}">About</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style} #{active?(contact_path)}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style} #{active?(blogs_path)}">Blog</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active?(portfolios_path)}">Portfolio</a></#{tag_type}>
+NAV
+    nav_links.html_safe
+  end
+
+  def active?(path)
+    "active" if current_page?(path)
+  end
+
 end
